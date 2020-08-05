b3c5bdc602bb9313d2409a773aa0cf666e446d16
David Sasser <david.alan.sasser@gmail.com>
2020-08-04 20:53:47 -0500

    initial commit

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+var pg = require('pg');
+require('dotenv').config();
+
+var pgPool = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+module.exports = pgPool;
